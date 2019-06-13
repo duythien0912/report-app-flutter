a62a7f8f8aa0c19543d86b3d2df42b64d4b41044
key46 <key46@key46s-MacBook-Air.local>
2019-06-13 17:08:52 +0700

    [Thien] add function pick image

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import 'new_feedback.dart';
@@ -70,28 +73,59 @@ class _ChatPageState extends State<ChatPage> {
                 "assets/police.png",
               ),
               onPressed: () {
-                launch("tel://21213123123");
-              },
-            ),
-          ),
-        ),
-        ClipOval(
-          child: Container(
-            color: Colors.green,
-            child: IconButton(
-              icon: Icon(Icons.feedback, color: Colors.white),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CustomFeedback(),
-                  ),
-                );
+                launch("tel://84981722293");
               },
             ),
           ),
         ),
       ],
+      floatingActionButton: Container(
+        // width: 200.0,
+        decoration: BoxDecoration(
+          color: Colors.green,
+          borderRadius: BorderRadius.circular(
+            12.0,
+          ),
+        ),
+        // height: 200.0,
+        child: RawMaterialButton(
+          shape: new CircleBorder(),
+          elevation: 0.0,
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => CustomFeedback(),
+              ),
+            );
+          },
+          child: IntrinsicWidth(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.speaker_notes,
+                    color: Colors.white,
+                    size: 25,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 10),
+                  ),
+                  Text(
+                    "Gửi đánh giá mới",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
       body: Container(
         child: Column(
           children: <Widget>[
@@ -165,18 +199,23 @@ class CustomFeedback extends StatelessWidget {
 
             children: <Widget>[
               BackButton(),
-              Padding(
-                padding: EdgeInsets.only(top: 10),
-              ),
+              // Padding(
+              //   padding: const EdgeInsets.symmetric(
+              //     horizontal: 16.0,
+              //     // vertical: 16,
+              //   ),
+              //   child: Text(
+              //     "Với chúng tôi, mọi phản hồi đều đáng quý. Theo bạn, chúng tôi nên cải thiện điểm nào?",
+              //     style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              //   ),
+              // ),
+
               Padding(
                 padding: const EdgeInsets.symmetric(
                   horizontal: 16.0,
-                  // vertical: 16,
+                  vertical: 6,
                 ),
-                child: Text(
-                  "Với chúng tôi, mọi phản hồi đều đáng quý. Theo bạn, chúng tôi nên cải thiện điểm nào?",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                ),
+                child: new PickImageRow(),
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(
@@ -194,10 +233,11 @@ class CustomFeedback extends StatelessWidget {
                   child: MyThreeOptions(),
                 ),
               ),
+
               Padding(
                 padding: const EdgeInsets.symmetric(
                   horizontal: 16.0,
-                  vertical: 12,
+                  vertical: 6,
                 ),
                 child: TextField(
                   decoration: new InputDecoration(
@@ -210,10 +250,11 @@ class CustomFeedback extends StatelessWidget {
                   ),
                 ),
               ),
+
               Padding(
                 padding: const EdgeInsets.symmetric(
                   horizontal: 16.0,
-                  vertical: 12,
+                  vertical: 6,
                 ),
                 child: Container(
                   // padding: EdgeInsets.all(8),
@@ -244,6 +285,146 @@ class CustomFeedback extends StatelessWidget {
   }
 }
 
+class PickImageRow extends StatefulWidget {
+  const PickImageRow({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _PickImageRowState createState() => _PickImageRowState();
+}
+
+enum ConfirmAction { CAMERA, GALLERY }
+
+class _PickImageRowState extends State<PickImageRow> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          "Ảnh báo cáo",
+          style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 6,
+          ),
+        ),
+        Wrap(
+          spacing: 6,
+          runSpacing: 6,
+          children: <Widget>[
+            new UploadBox(),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class UploadBox extends StatefulWidget {
+  const UploadBox({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _UploadBoxState createState() => _UploadBoxState();
+}
+
+class _UploadBoxState extends State<UploadBox> {
+  File _image;
+
+  void showDemoActionSheet({BuildContext context, Widget child}) {
+    showCupertinoModalPopup<String>(
+      context: context,
+      builder: (BuildContext context) => child,
+    ).then((String value) {
+      if (value != null) {
+        // setState(() { lastSelectedValue = value; });
+      }
+    });
+  }
+
+  Future getImage() async {
+    File image;
+
+    showDemoActionSheet(
+      context: context,
+      child: CupertinoActionSheet(
+        title: const Text('Chọn ảnh'),
+        // message: const Text(
+        //     'Please select the best dessert from the options below.'),
+        actions: <Widget>[
+          CupertinoActionSheetAction(
+            child: const Text('Máy ảnh'),
+            onPressed: () async {
+              Navigator.pop(context, 'Máy ảnh');
+              image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+              setState(() {
+                _image = image;
+              });
+            },
+          ),
+          CupertinoActionSheetAction(
+            child: const Text('Thư viện'),
+            onPressed: () async {
+              Navigator.pop(context, 'Thư viện');
+              image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+              setState(() {
+                _image = image;
+              });
+            },
+          ),
+        ],
+        cancelButton: CupertinoActionSheetAction(
+          child: const Text('Hủy bỏ'),
+          isDefaultAction: true,
+          onPressed: () {
+            Navigator.pop(context, 'Cancel');
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
+    double widthMinus = width - 16 * 2 - 6 * 3;
+    // _image != null ? Image.file(_image) : Container(),
+
+    return GestureDetector(
+      onTap: () {
+        getImage();
+      },
+      child: Container(
+        height: widthMinus / 4,
+        width: widthMinus / 4,
+        // color: Colors.red,
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 1,
+            color: Colors.green,
+            style: BorderStyle.solid,
+          ),
+          borderRadius: BorderRadius.circular(
+            6.0,
+          ),
+        ),
+        child: Icon(
+          Icons.add,
+          color: Colors.green,
+        ),
+      ),
+    );
+  }
+}
+
 class SupportItem extends StatelessWidget {
   const SupportItem({
     Key key,
@@ -256,10 +437,10 @@ class SupportItem extends StatelessWidget {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     // double widthMinus = width - 56 - 78 - 72;
-    double widthMinus = width - 56 - 78 - 25;
+    double widthMinus = width - 16 * 2 - 11 * 3;
     return Container(
-      height: widthMinus / 3 + 24,
-      width: widthMinus / 3,
+      height: widthMinus / 4 + 8,
+      width: widthMinus / 4,
       // color: Colors.teal,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -269,17 +450,9 @@ class SupportItem extends StatelessWidget {
               ? FlutterLogo()
               : Image.asset(
                   item.image,
-                  height: widthMinus / 3,
+                  height: widthMinus / 4,
+                  fit: BoxFit.contain,
                 ),
-          Padding(
-            padding: EdgeInsets.only(top: 7),
-          ),
-          Text(
-            item.name,
-            style: TextStyle(
-              fontSize: 12,
-            ),
-          )
         ],
       ),
     );
@@ -287,11 +460,11 @@ class SupportItem extends StatelessWidget {
 }
 
 class SupItem {
+  SupItem({this.index, this.name, this.image});
+
+  final String image;
   final int index;
   final String name;
-  final String image;
-
-  SupItem({this.index, this.name, this.image});
 }
 
 class MyThreeOptions extends StatefulWidget {
@@ -299,78 +472,99 @@ class MyThreeOptions extends StatefulWidget {
   _MyThreeOptionsState createState() => _MyThreeOptionsState();
 }
 
-List<SupItem> reportList = [
-  SupItem(index: 1, image: "assets/bus.png", name: "Xe"),
-  SupItem(index: 2, image: "assets/driver1.png", name: "Tài Xế"),
-  SupItem(index: 3, image: "assets/bus_stop1.png", name: "Bến"),
-  SupItem(index: 4, image: "assets/app1.png", name: "Ứng dụng"),
-  SupItem(index: 5, image: "assets/404.png", name: "Khác"),
-];
-
 class _MyThreeOptionsState extends State<MyThreeOptions> {
   List<SupItem> selectedChoices = [];
 
+  List<SupItem> reportList = [
+    SupItem(index: 1, image: "assets/bus.png", name: "Xe"),
+    SupItem(index: 2, image: "assets/driver1.png", name: "Tài Xế"),
+    SupItem(index: 3, image: "assets/bus_stop1.png", name: "Bến"),
+    SupItem(index: 4, image: "assets/app1.png", name: "Ứng dụng"),
+    SupItem(index: 5, image: "assets/404.png", name: "Khác"),
+  ];
   // List<int> selectedChoices = List();
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
+    double widthMinus = width - 16 * 2 - 11 * 3;
+
     return Wrap(
       spacing: 11,
       runSpacing: 11,
       children: List<Widget>.generate(
         reportList.length,
         (int index) {
-          return ChoiceChip(
-            // avatar: SupportItem(),
+          return Container(
+            width: widthMinus / 4,
+            child: Column(
+              children: <Widget>[
+                ChoiceChip(
+                  // avatar: SupportItem(),
 
-            clipBehavior: Clip.none,
-            // padding: EdgeInsets.all(0),
-            shape: ShapeBorder.lerp(
-                RoundedRectangleBorder(
-                  side: BorderSide(
-                    color: selectedChoices.contains(reportList[index])
-                        ? Color(0xFF00800d)
-                        : Colors.transparent,
-                    width: 1.5,
-                  ),
-                  // side: const BorderSide(width: 1.0),
-                  borderRadius: BorderRadius.circular(
-                    8.0,
-                  ),
-                ),
-                RoundedRectangleBorder(
-                  side: BorderSide(
-                    color: selectedChoices.contains(reportList[index])
-                        ? Color(0xFF00800d)
-                        : Colors.transparent,
-                    width: 1,
-                  ),
-                  // side: const BorderSide(width: 1.0),
-                  borderRadius: BorderRadius.circular(
-                    8.0,
-                  ),
-                ),
-                0.0),
-            backgroundColor: Colors.grey[200],
-            shadowColor: Colors.transparent,
-            selectedShadowColor: Colors.transparent,
-            padding: EdgeInsets.all(8),
-            selectedColor: Colors.grey[200],
-            label: SupportItem(item: reportList[index]),
-            selected: selectedChoices.contains(reportList[index]),
-            onSelected: (bool selected) {
-              // setState(() {
-              //   _value = selected ? index : null;
-              // });
-              selectedChoices.contains(reportList[index])
-                  ? selectedChoices.remove(reportList[index])
-                  : selectedChoices.add(reportList[index]);
-              setState(() {
-                selectedChoices = selectedChoices;
-              });
+                  clipBehavior: Clip.none,
+                  padding: EdgeInsets.all(0),
+                  shape: ShapeBorder.lerp(
+                      RoundedRectangleBorder(
+                        side: BorderSide(
+                          color: selectedChoices.contains(reportList[index])
+                              ? Color(0xFF00800d)
+                              : Colors.transparent,
+                          width: 1.5,
+                        ),
+                        // side: const BorderSide(width: 1.0),
+                        borderRadius: BorderRadius.circular(
+                          8.0,
+                        ),
+                      ),
+                      RoundedRectangleBorder(
+                        side: BorderSide(
+                          color: selectedChoices.contains(reportList[index])
+                              ? Color(0xFF00800d)
+                              : Colors.transparent,
+                          width: 1,
+                        ),
+                        // side: const BorderSide(width: 1.0),
+                        borderRadius: BorderRadius.circular(
+                          8.0,
+                        ),
+                      ),
+                      0.0),
+                  backgroundColor: Colors.grey[200],
+                  shadowColor: Colors.transparent,
+                  selectedShadowColor: Colors.transparent,
+                  // padding: EdgeInsets.all(8),
+                  selectedColor: Colors.grey[200],
+                  label: SupportItem(item: reportList[index]),
+                  selected: selectedChoices.contains(reportList[index]),
+                  onSelected: (bool selected) {
+                    // setState(() {
+                    //   _value = selected ? index : null;
+                    // });
+                    selectedChoices.contains(reportList[index])
+                        ? selectedChoices.remove(reportList[index])
+                        : selectedChoices.add(reportList[index]);
+                    setState(() {
+                      selectedChoices = selectedChoices;
+                    });
 
-              print(selectedChoices);
-            },
+                    print(selectedChoices);
+                  },
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 7),
+                ),
+                Text(
+                  reportList[index].name,
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                  overflow: TextOverflow.visible,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
           );
         },
       ).toList(),
