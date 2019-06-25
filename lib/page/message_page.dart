1f670460d9aa53519b681d85e2fecc4389624026
key46 <key46@key46s-MacBook-Air.local>
2019-06-25 10:16:29 +0700

    [Thien] add chat UI

--- lib/page/message_page.dart
+++ lib/page/message_page.dart
@@ -5,12 +5,147 @@ class MessagePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return ChatScreen();
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  ChatScreen({Key key}) : super(key: key);
+
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = TextEditingController();
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  bool _isComposing = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _textController.addListener(_printLatestValue);
+  }
+
+  _printLatestValue() {
+    print("text field: ${_textController.text}");
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Phản ánh số 01"),
+        centerTitle: true,
       ),
-      body: Container(),
-      // child: child,
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+          Divider(height: 1.0),
+          Container(
+            decoration: BoxDecoration(color: Theme.of(context).cardColor),
+            child: _buildTextComposer(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                autofocus: true,
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                onChanged: (String text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
+                decoration:
+                    InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: _isComposing
+                    ? () => _handleSubmitted(_textController.text)
+                    : null,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    print(text);
+    if (_textController.text.isNotEmpty) {
+      ChatMessage message = ChatMessage(
+        text: text,
+      );
+      _textController.clear();
+      _textController.text = '';
+      _textController.clearComposing();
+
+      setState(() {
+        _messages.insert(0, message);
+      });
+    }
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    const String _name = "Your Name";
+
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0])),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(_name, style: Theme.of(context).textTheme.subhead),
+                Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: Text(text),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
